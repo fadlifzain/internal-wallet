376b615635d3931364b7f4a49d8b6e5c19f546c6
Fadli Fauzi Zain <fadlifzain@gmail.com>
2023-11-08 09:46:19 +0700

    beautify migration

--- db/migrate/20231030013020_create_wallets.rb
+++ db/migrate/20231030013020_create_wallets.rb
@@ -1,8 +1,8 @@
 class CreateWallets < ActiveRecord::Migration[7.1]
   def change
     create_table :wallets do |t|
-      t.string   :wallet_id
-      t.monetize :balance, default: 0
+      t.string    :wallet_id
+      t.monetize  :balance, default: 0
 
       t.references :account
       t.timestamps
